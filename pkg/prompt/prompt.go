d59f9ca19d737679f36061525b5a8b0dbd4bd53b
kensukeshouni <6532835+shouni@users.noreply.github.com>
2026-01-05 05:58:26 +0900

    Comment out unused manga title line in prompt generation to declutter the output.

--- pkg/prompt/prompt.go
+++ pkg/prompt/prompt.go
@@ -31,7 +31,7 @@ func (pb *PromptBuilder) BuildFullPagePrompt(mangaTitle string, pages []domain.M
 	sb.WriteString(fmt.Sprintf("\n- TOTAL PANELS: This page MUST contain exactly %d distinct panels.\n", len(pages)))
 
 	// 2. タイトルと共通スタイルの適用
-	sb.WriteString(fmt.Sprintf("\n### TITLE: %s ###\n", mangaTitle))
+	//	sb.WriteString(fmt.Sprintf("\n### TITLE: %s ###\n", mangaTitle))
 	sb.WriteString(RenderingStyle)
 	if pb.defaultSuffix != "" {
 		sb.WriteString(fmt.Sprintf("- STYLE_DNA: %s\n", pb.defaultSuffix))
