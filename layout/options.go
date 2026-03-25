a7a909c34a6501b15837b400e845a63cfb9977db
kensukeshouni <6532835+shouni@users.noreply.github.com>
2026-03-26 08:06:44 +0900

    Refactor: Add input validation for concurrency, rate interval, and burst values in panel and page option methods

--- layout/options.go
+++ layout/options.go
@@ -10,21 +10,27 @@ type PanelOption func(*PanelGenerator)
 // WithPanelMaxConcurrency は、パネル生成の最大並列数を設定します。
 func WithPanelMaxConcurrency(value int64) PanelOption {
 	return func(g *PanelGenerator) {
-		g.maxConcurrency = value
+		if value > 0 {
+			g.maxConcurrency = value
+		}
 	}
 }
 
 // WithPanelRateInterval は、パネル生成のレートリミット間隔を設定します。
 func WithPanelRateInterval(d time.Duration) PanelOption {
 	return func(g *PanelGenerator) {
-		g.rateInterval = d
+		if d > 0 {
+			g.rateInterval = d
+		}
 	}
 }
 
 // WithPanelRateBurst は、パネル生成のバースト許容数を設定します。
 func WithPanelRateBurst(value int) PanelOption {
 	return func(g *PanelGenerator) {
-		g.rateBurst = value
+		if value > 0 {
+			g.rateBurst = value
+		}
 	}
 }
 
@@ -36,27 +42,35 @@ type PageOption func(*PageGenerator)
 // WithPageMaxConcurrency は、ページ生成の最大並列数を設定します。
 func WithPageMaxConcurrency(value int64) PageOption {
 	return func(g *PageGenerator) {
-		g.maxConcurrency = value
+		if value > 0 {
+			g.maxConcurrency = value
+		}
 	}
 }
 
 // WithPageRateInterval は、ページ生成のレートリミット間隔を設定します。
 func WithPageRateInterval(d time.Duration) PageOption {
 	return func(g *PageGenerator) {
-		g.rateInterval = d
+		if d > 0 {
+			g.rateInterval = d
+		}
 	}
 }
 
 // WithPageRateBurst は、ページ生成のバースト許容数を設定します。
 func WithPageRateBurst(value int) PageOption {
 	return func(g *PageGenerator) {
-		g.rateBurst = value
+		if value > 0 {
+			g.rateBurst = value
+		}
 	}
 }
 
 // WithMaxPanelsPerPage は、1ページあたりの最大パネル数を設定します。
 func WithMaxPanelsPerPage(value int) PageOption {
 	return func(g *PageGenerator) {
-		g.maxPanelsPerPage = value
+		if value > 0 {
+			g.maxPanelsPerPage = value
+		}
 	}
 }
